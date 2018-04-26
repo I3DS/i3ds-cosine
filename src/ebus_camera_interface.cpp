6ff2b26826eccebeabeb92ca44c011ea544ea0d7
Espen Helle <espen.helle@sintef>
2018-04-26 16:22:46 +0200

    Just copy n paste of straming code to have an initial state. (Not compiling)

--- src/ebus_camera_interface.cpp
+++ src/ebus_camera_interface.cpp
@@ -495,3 +495,306 @@ void EbusCameraInterface::setRegion(PlanarRegion region){
     return region;
   */
 }
+
+
+
+
+
+
+
+
+-/////-------------------------------- Streaming part
+
+
+//
+// Opens stream, pipeline, allocates buffers
+//
+
+bool EBusInterface::OpenStream() {
+	std::cout << "--> OpenStream" << std::endl;
+
+	// Creates and open the stream object based on the selected device.
+	PvResult lResult = PvResult::Code::INVALID_PARAMETER;
+	mStream = PvStream::CreateAndOpen(mConnectionID, &lResult);
+	if (!lResult.IsOK()) {
+		std::cout << "Unable to open the stream" << std::endl;
+		return false;
+	}
+
+	mPipeline = new PvPipeline(mStream);
+
+	// Reading payload size from device
+	int64_t lSize = mDevice->GetPayloadSize();
+
+	// Create, init the PvPipeline object
+	mPipeline->SetBufferSize(static_cast<uint32_t>(lSize));
+	mPipeline->SetBufferCount(16);
+
+	// The pipeline needs to be "armed", or started before  we instruct the device to send us images
+	lResult = mPipeline->Start();
+	if (!lResult.IsOK()) {
+		std::cout << "Unable to start pipeline" << std::endl;
+		return false;
+	}
+
+	// Only for GigE Vision, if supported
+	PvGenBoolean *lRequestMissingPackets =
+			dynamic_cast<PvGenBoolean *>(mStream->GetParameters()->GetBoolean(
+					"RequestMissingPackets"));
+	if ((lRequestMissingPackets != NULL)
+			&& lRequestMissingPackets->IsAvailable()) {
+		// Disabling request missing packets.
+		lRequestMissingPackets->SetValue(false);
+	}
+
+	return true;
+}
+
+//
+// Closes the stream, pipeline
+//
+
+void EBusInterface::CloseStream() {
+	std::cout << "--> CloseStream" << std::endl;
+
+	if (mPipeline != NULL) {
+		if (mPipeline->IsStarted()) {
+			if (!mPipeline->Stop().IsOK()) {
+				std::cout << "Unable to stop the pipeline." << std::endl;
+			}
+		}
+
+		delete mPipeline;
+		mPipeline = NULL;
+	}
+
+	if (mStream != NULL) {
+		if (mStream->IsOpen()) {
+			if (!mStream->Close().IsOK()) {
+				std::cout << "Unable to stop the stream." << std::endl;
+			}
+		}
+
+		PvStream::Free(mStream);
+		mStream = NULL;
+	}
+}
+
+//
+// Starts image acquisition
+//
+
+bool EBusInterface::StartAcquisition() {
+	std::cout << "--> StartAcquisition" << std::endl;
+
+	// Flush packet queue to make sure there is no left over from previous disconnect event
+	PvStreamGEV* lStreamGEV = dynamic_cast<PvStreamGEV*>(mStream);
+	if (lStreamGEV != NULL) {
+		lStreamGEV->FlushPacketQueue();
+	}
+
+	// Set streaming destination (only GigE Vision devces)
+	PvDeviceGEV* lDeviceGEV = dynamic_cast<PvDeviceGEV*>(mDevice);
+	if (lDeviceGEV != NULL) {
+		// If using a GigE Vision, it is same to assume the stream object is GigE Vision as well
+		PvStreamGEV* lStreamGEV = static_cast<PvStreamGEV*>(mStream);
+
+		// Have to set the Device IP destination to the Stream
+		PvResult lResult = lDeviceGEV->SetStreamDestination(
+				lStreamGEV->GetLocalIPAddress(), lStreamGEV->GetLocalPort());
+		if (!lResult.IsOK()) {
+			std::cout << "Setting stream destination failed" << std::endl;
+			return false;
+		}
+	}
+
+	// Enables stream before sending the AcquisitionStart command.
+	mDevice->StreamEnable();
+
+	// The pipeline is already "armed", we just have to tell the device to start sending us images
+	PvResult lResult = mDevice->GetParameters()->ExecuteCommand(
+			"AcquisitionStart");
+	if (!lResult.IsOK()) {
+		std::cout << "Unable to start acquisition" << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+//
+// Stops acquisition
+//
+
+bool EBusInterface::StopAcquisition() {
+	std::cout << "--> StopAcquisition" << std::endl;
+
+	// Tell the device to stop sending images.
+	mDevice->GetParameters()->ExecuteCommand("AcquisitionStop");
+
+	// Disable stream after sending the AcquisitionStop command.
+	mDevice->StreamDisable();
+
+	PvDeviceGEV* lDeviceGEV = dynamic_cast<PvDeviceGEV*>(mDevice);
+	if (lDeviceGEV != NULL) {
+		// Reset streaming destination (optional...)
+		lDeviceGEV->ResetStreamDestination();
+	}
+
+	return true;
+}
+
+//
+// Acquisition loop
+//
+
+void EBusInterface::ApplicationLoop() {
+	std::cout << "--> ApplicationLoop" << std::endl;
+
+	char lDoodle[] = "|\\-|-/";
+	int lDoodleIndex = 0;
+	bool lFirstTimeout = true;
+
+	int64_t lImageCountVal = 0;
+	double lFrameRateVal = 0.0;
+	double lBandwidthVal = 0.0;
+
+	// Acquire images until the user instructs us to stop.
+	while (!PvKbHit()) {
+		// If connection flag is up, teardown device/stream
+		if (mConnectionLost && (mDevice != NULL)) {
+			// Device lost: no need to stop acquisition
+			TearDown(false);
+		}
+
+		// If the device is not connected, attempt reconnection
+		if (mDevice == NULL) {
+			if (ConnectDevice()) {
+				// Device is connected, open the stream
+				if (OpenStream()) {
+					// Device is connected, stream is opened: start acquisition
+					if (!StartAcquisition()) {
+						TearDown(false);
+					}
+				} else {
+					TearDown(false);
+				}
+			}
+		}
+
+		// If still no device, no need to continue the loop
+		if (mDevice == NULL) {
+			continue;
+		}
+
+		if ((mStream != NULL) && mStream->IsOpen() && (mPipeline != NULL)
+				&& mPipeline->IsStarted()) {
+			// Retrieve next buffer
+			PvBuffer *lBuffer = NULL;
+			PvResult lOperationResult;
+			PvResult lResult = mPipeline->RetrieveNextBuffer(&lBuffer, 1000,
+					&lOperationResult);
+
+			if (lResult.IsOK()) {
+				if (lOperationResult.IsOK()) {
+					//
+					// We now have a valid buffer. This is where you would typically process the buffer.
+					// -----------------------------------------------------------------------------------------
+					// ...
+
+					mStream->GetParameters()->GetIntegerValue("BlockCount",
+							lImageCountVal);
+					mStream->GetParameters()->GetFloatValue("AcquisitionRate",
+							lFrameRateVal);
+					mStream->GetParameters()->GetFloatValue("Bandwidth",
+							lBandwidthVal);
+
+					// If the buffer contains an image, display width and height.
+					uint32_t lWidth = 0, lHeight = 0;
+					if (lBuffer->GetPayloadType() == PvPayloadTypeImage) {
+						// Get image specific buffer interface.
+						PvImage *lImage = lBuffer->GetImage();
+
+						// Read width, height.
+						lWidth = lImage->GetWidth();
+						lHeight = lImage->GetHeight();
+					}
+
+					std::cout << fixed << setprecision(1);
+					std::cout << lDoodle[lDoodleIndex];
+					std::cout << " BlockID: " << uppercase << hex
+							<< setfill('0') << setw(16) << lBuffer->GetBlockID()
+							<< " W: " << dec << lWidth << " H: " << lHeight
+							<< " " << lFrameRateVal << " FPS "
+							<< (lBandwidthVal / 1000000.0) << " Mb/s  \r";
+
+					lFirstTimeout = true;
+				}
+				// We have an image - do some processing (...) and VERY IMPORTANT,
+				// release the buffer back to the pipeline.
+				mPipeline->ReleaseBuffer(lBuffer);
+			} else {
+				// Timeout
+
+				if (lFirstTimeout) {
+					std::cout << "" << std::endl;
+					lFirstTimeout = false;
+				}
+
+				std::cout << "Image timeout " << lDoodle[lDoodleIndex]
+						<< std::endl;
+			}
+
+			++lDoodleIndex %= 6;
+		} else {
+			// No stream/pipeline, must be in recovery. Wait a bit...
+			PvSleepMs(100);
+		}
+	}
+
+	PvGetChar(); // Flush key buffer for next stop.
+	std::cout << "" << std::endl;
+}
+
+//
+// \brief Disconnects the device
+//
+
+void EBusInterface::DisconnectDevice() {
+	std::cout << "--> DisconnectDevice" << std::endl;
+
+	if (mDevice != NULL) {
+		if (mDevice->IsConnected()) {
+			// Unregister event sink (call-backs).
+			mDevice->UnregisterEventSink(this);
+		}
+
+		PvDevice::Free(mDevice);
+		mDevice = NULL;
+	}
+}
+
+//
+// Tear down: closes, disconnects, etc.
+//
+
+void EBusInterface::TearDown(bool aStopAcquisition) {
+	std::cout << "--> TearDown" << std::endl;
+
+	if (aStopAcquisition) {
+		StopAcquisition();
+	}
+
+	CloseStream();
+	DisconnectDevice();
+}
+
+//
+// PvDeviceEventSink callback
+//
+// Notification that the device just got disconnected.
+//
+
+
+
+
